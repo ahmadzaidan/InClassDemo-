afb3620acb9886bc7f972132e70d1368f260d4cd
ahmadzaidan <ahmad.zaidan001@umb.edu>
2015-12-12 22:52:52 -0500

    edited models .py modified it added question(Whats up) also date published etc."

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -4,10 +4,23 @@ from django.contrib import admin
 from .models import Choice, Question
 
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
-    ]
-admin.site.register(Choice)
+    # ...
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'    
+    list_filter = ['pub_date']
+
+
+    inlines = [ChoiceInline]
+
+
 admin.site.register(Question, QuestionAdmin)
